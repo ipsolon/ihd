bd5604fabdc7e8a965abd2835dd41085c0ce86d8
Joshua Meyers <joshua.meyers@bluehalo.com>
2024-06-25 16:50:31 -0400

    Added UHD dissector build to docker and added an option to mount a UHD version in the docker at runtime

--- docker/uhd-gnuradio-latest/docker_run.sh
+++ docker/uhd-gnuradio-latest/docker_run.sh
@@ -1,4 +1,19 @@
+#!/bin/bash
+
+MOUNT_UHD=""
+if [ -z "$1" ]; then
+    echo "Provide path to mount uhd source if desired"
+else
+  if [ -d "$1" ]; then
+    MOUNT_UHD='--mount type=bind,source='$1',target=/opt/uhd'
+    echo "Mount UHD source: $MOUNT_UHD"
+  fi
+fi
+
 docker run --net=host --env="DISPLAY" -e LOCAL_UID=$(id -u $USER) -e LOCAL_GID=$(id -g $USER) \
   --volume="$HOME/.Xauthority:/root/.Xauthority:rw" --privileged --privileged -v /dev/bus/usb/:/dev/bus/usb/ \
-  --device /dev/snd --mount type=bind,source="$(pwd)"/../../../ihd,target=/ihd --group-add=audio \
+  --device /dev/snd \
+  --mount type=bind,source="$(pwd)"/../../../ihd,target=/ihd \
+  ${MOUNT_UHD} \
+  --group-add=audio \
   -it ubuntu:gnuradio-releases-latest bash
