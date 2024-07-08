184c1f5f9379d4746f93b2d29b6d29d5640f6553
Joshua Meyers <joshua.meyers@bluehalo.com>
2024-07-08 16:46:19 -0400

    get gnuradio to run.  Not at all correct, just running.

--- gnuradio/gr-ihd/lib/ihd_source_impl.cc
+++ gnuradio/gr-ihd/lib/ihd_source_impl.cc
@@ -8,10 +8,11 @@
 #include "ihd_source_impl.h"
 #include <gnuradio/io_signature.h>
 
-namespace gr {
-namespace ihd {
+namespace gr::ihd {
 constexpr size_t samples = 4096;
-constexpr size_t nchan = 1;
+constexpr size_t n_chan = 1;
+constexpr size_t n_inputs = 1;
+constexpr int NUMBER_OF_STREAM_ITEMS = 4; /* Not at all correct, just getting GNU Radio to run */
 
 ihd_source::sptr ihd_source::make(double center_freq)
 {
@@ -20,6 +21,7 @@ ihd_source::sptr ihd_source::make(double center_freq)
 
 bool ihd_source_impl::start()
 {
+    d_logger->debug("IHD starting");
     if (_issue_stream_cmd_on_start) {
         // Start the streamers
         ::uhd::stream_cmd_t stream_cmd(::uhd::stream_cmd_t::STREAM_MODE_START_CONTINUOUS);
@@ -36,6 +38,7 @@ bool ihd_source_impl::start()
     } else {
         d_logger->debug("Starting RX streamer without stream command...");
     }
+    d_logger->debug("IHD done starting");
     return true;
 }
 
@@ -57,13 +60,15 @@ ihd_source_impl::ihd_source_impl(double center_freq)
     : gr::sync_block("ihd_source",
                      gr::io_signature::make(0, 0, 0),
                      gr::io_signature::make(
-                     1 /* min outputs */, 1 /*max outputs */, nchan)),
+                     n_inputs /* min outputs */,
+                                            n_chan /*max outputs */, NUMBER_OF_STREAM_ITEMS)),
     _issue_stream_cmd_on_start(true),
     _stream_args("sc16", "sc16")
 {
+    d_logger->debug("Start constructing IHD");
     _center_freq = center_freq;
     std::string args("addr=10.75.42.15");
-    size_t channel;
+    size_t channel = 0;
 
     _isrp = ::ihd::ipsolon_isrp::make(args);
     uhd::tune_request_t tune_request{};
@@ -76,6 +81,7 @@ ihd_source_impl::ihd_source_impl(double center_freq)
     _stream_args.channels = channel_nums;
     _streamer = _isrp->get_rx_stream(_stream_args);
 
+    d_logger->debug("Done constructing IHD");
 
 #if 0
     sleep(5);
@@ -87,18 +93,20 @@ ihd_source_impl::ihd_source_impl(double center_freq)
 /*
  * Our virtual destructor.
  */
-ihd_source_impl::~ihd_source_impl() {}
+ihd_source_impl::~ihd_source_impl() = default;
 
 int ihd_source_impl::work(int noutput_items,
                           gr_vector_const_void_star& input_items,
                           gr_vector_void_star& output_items)
 {
+    d_logger->debug("Working.  number output_items:%d", noutput_items);
     std::vector<uint8_t> buffs = std::vector<uint8_t>(samples);
     output_items.push_back(&buffs);
-
+    usleep(1000);
+    
     // Tell runtime system how many output items we produced.
     return noutput_items;
 }
 
-} /* namespace ihd */
-} /* namespace gr */
+} // namespace gr::ihd
+
