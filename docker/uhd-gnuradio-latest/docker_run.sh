75d54cfd1eb33c3c8c238bd5f0202403ada440b4
Joshua Meyers <joshua.meyers@bluehalo.com>
2024-09-11 12:11:32 -0400

    Fix docker run script so that it does not create a new container everytime

--- docker/uhd-gnuradio-latest/docker_run.sh
+++ docker/uhd-gnuradio-latest/docker_run.sh
@@ -16,9 +16,21 @@ if [[ "$OSTYPE" == "msys"* ]]; then
   WINDOWS_RUN=winpty
 fi
 
-$WINDOWS_RUN docker run --net=host --env="DISPLAY" -e LOCAL_UID=$(id -u $USER) -e LOCAL_GID=$(id -g $USER) \
-  --volume="$HOME/.Xauthority:/root/.Xauthority:rw" --privileged --privileged -v /dev/bus/usb/:/dev/bus/usb/ \
-  --mount type=bind,source="$(pwd)"/../../../ihd,target=/ihd \
-  ${MOUNT_OPT} \
-  --group-add=audio \
-  -it ubuntu:gnuradio-releases-latest bash
+CONTAINER_NAME="gnuradio-releases-latest_$USER"
+ CID=$(docker ps -aq -f name=^/${CONTAINER_NAME}$)
+if [ ! "${CID}" ]; then
+  echo "Container ${CONTAINER_NAME} does not exist, create it";
+  $WINDOWS_RUN docker run --net=host --env="DISPLAY" -e LOCAL_UID=$(id -u $USER) -e LOCAL_GID=$(id -g $USER) \
+    --volume="$HOME/.Xauthority:/root/.Xauthority:rw" --privileged --privileged -v /dev/bus/usb/:/dev/bus/usb/ \
+    --mount type=bind,source="$(pwd)"/../../../ihd,target=/ihd \
+    ${MOUNT_OPT} \
+    --group-add=audio \
+    --name $CONTAINER_NAME \
+    -it ubuntu:gnuradio-releases-latest bash
+else
+  echo "Container ${CONTAINER_NAME} does exist, start and run it";
+  docker start ${CONTAINER_NAME}
+  docker attach ${CONTAINER_NAME}
+fi
+unset CID
+unset CONTAINER_NAME
