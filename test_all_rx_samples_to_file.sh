49ee119b887857dc7eac4116dbbafee0c4d2f3b0
John Plaschke <john.plaschke@bluehalo.com>
2025-01-07 13:07:51 -0500

    fixed with nsamps=100000

--- test_all_rx_samples_to_file.sh
+++ test_all_rx_samples_to_file.sh
@@ -1,13 +1,13 @@
 #!/bin/bash 
 #if [ $# -eq 0 ];`
 #then
-let "NUM_SAMPS=1000000"
+let "NUM_SAMPS=100000"
 #fi
 #if [ $#1 -eq 1 ];
 #    let "NUM_SAMPS=$1";
 #fi
 
-FILE_SIZE_256_FFT=399360
+let "FILE_SIZE_256_FFT=399360"
 FILENAME=isrp_samples.dat
 let "errors=0"
 let "file_size_errors=0"
@@ -15,29 +15,30 @@ echo -e "\n\n\n***** psd fft_size=256 channel = 1"
 rm isrp_samples.dat
 ./rx_samples_to_file --dest_ip=10.75.42.248 --args=addr=10.75.42.202 --stream_type=psd --nsamps="$NUM_SAMPS" --fft_size=256 --channel=1
 ret_code=$?
-if [ $ret_code != 0 ]; then let "errors=errors+1";  echo -e "***ERROR\n\n"; fi
+if [ $ret_code != 0 ]; then let "errors=errors+1";  echo -e "***ERROR1\n\n"; fi
 ls -al isrp_samples.dat
 FILESIZE=$(stat -c%s "$FILENAME")
-if [[ $FILESIZE != $FILE_SIZE_256_FFT ]]; then let "file_size_errors=file_fize_error+1"; echo -e "\n***ERROR \n\n"; fi
+echo "FILESIZE FFT FILE $FILESIZE SHOULD BE $FILE_SIZE_256_FFT"
+if [[ $FILESIZE != $FILE_SIZE_256_FFT ]]; then let "file_size_errors=file_size_error+1"; echo -e "\n***ERROR2 \n\n"; fi
 
 echo -e "\n\n\n***** psd fft_size=256 channel = 2"
 rm isrp_samples.dat
 ./rx_samples_to_file --dest_ip=10.75.42.248 --args=addr=10.75.42.202 --stream_type=psd --nsamps="$NUM_SAMPS" --fft_size=256 --channel=2
 ret_code=$?
-if [ $ret_code != 0 ]; then let "errors=errors+1"; echo -e "***ERROR\n\n"; fi
+if [ $ret_code != 0 ]; then let "errors=errors+1"; echo -e "***ERROR3\n\n"; fi
 ls -al isrp_samples.dat
 FILESIZE=$(stat -c%s "$FILENAME")
 echo "FILESIZE = ${FILESIZE}"
-if [[ $FILESIZE != $FILE_SIZE_256_FFT ]]; then let "file_size_errors=file_fize_error+1"; echo -e "\n***ERROR \n\n"; fi
+if [[ $FILESIZE != $FILE_SIZE_256_FFT ]]; then let "file_size_errors=file_size_error+1"; echo -e "\n***ERROR4 \n\n"; fi
 
 echo -e "\n\n\n***** psd fft_size=256 channel = 3"
 rm isrp_samples.dat
 ./rx_samples_to_file --dest_ip=10.75.42.248 --args=addr=10.75.42.202 --stream_type=psd --nsamps="$NUM_SAMPS" --fft_size=256 --channel=3
 ret_code=$?
-if [ $ret_code != 0 ]; then let "errors=errors+1";  echo -e "***ERROR\n\n"; fi
+if [ $ret_code != 0 ]; then let "errors=errors+1";  echo -e "***ERROR5\n\n"; fi
 ls -al isrp_samples.dat
 FILESIZE=$(stat -c%s "$FILENAME")
-if [[ $FILESIZE != $FILE_SIZE_256_FFT ]]; then let "file_size_errors=file_fize_error+1"; echo -e "\n***ERROR \n\n"; fi
+if [[ $FILESIZE != $FILE_SIZE_256_FFT ]]; then let "file_size_errors=file_size_error+1"; echo -e "\n***ERROR6 \n\n"; fi
 
 
 echo -e "\n\n\n***** psd fft_size=512 channel = 1"
@@ -45,29 +46,29 @@ FILE_SIZE_512_FFT=399360
 rm isrp_samples.dat
 ./rx_samples_to_file --dest_ip=10.75.42.248 --args=addr=10.75.42.202 --stream_type=psd --nsamps="$NUM_SAMPS" --fft_size=512 --channel=1
 ret_code=$?
-if [ $ret_code != 0 ]; then let "errors=errors+1";  echo -e "***ERROR\n\n"; fi
+if [ $ret_code != 0 ]; then let "errors=errors+1";  echo -e "***ERROR7\n\n"; fi
 ls -al isrp_samples.dat
 FILESIZE=$(stat -c%s "$FILENAME")
-if [[ $FILESIZE != $FILE_SIZE_512_FFT ]]; then let "file_size_errors=file_fize_error+1"; echo -e "\n***ERROR \n\n"; fi
+if [[ $FILESIZE != $FILE_SIZE_512_FFT ]]; then let "file_size_errors=file_size_error+1"; echo -e "\n***ERROR8 \n\n"; fi
 
 echo -e "\n\n\n***** psd fft_size=512 channel = 2"
 rm isrp_samples.dat
 ./rx_samples_to_file --dest_ip=10.75.42.248 --args=addr=10.75.42.202 --stream_type=psd --nsamps="$NUM_SAMPS" --fft_size=512 --channel=2
 ret_code=$?
-if [ $ret_code != 0 ]; then let "errors=errors+1";  echo -e "*** ERROR\n\n"; fi
+if [ $ret_code != 0 ]; then let "errors=errors+1";  echo -e "*** ERROR9\n\n"; fi
 ls -al isrp_samples.dat
 FILESIZE=$(stat -c%s "$FILENAME")
-if [[ $FILESIZE != $FILE_SIZE_512_FFT ]]; then let "file_size_errors=file_fize_error+1"; echo -e "\n***ERROR \n\n"; fi
+if [[ $FILESIZE != $FILE_SIZE_512_FFT ]]; then let "file_size_errors=file_size_error+1"; echo -e "\n***ERROR10 \n\n"; fi
 
 
 echo -e "\n\n\n***** psd fft_size=512 channel = 3"
 rm isrp_samples.dat
 ./rx_samples_to_file --dest_ip=10.75.42.248 --args=addr=10.75.42.202 --stream_type=psd --nsamps="$NUM_SAMPS" --fft_size=512 --channel=3
 ret_code=$?
-if [ $ret_code != 0 ]; then let "errors=errors+1"; echo -e "*** ERROR\n\n"; fi
+if [ $ret_code != 0 ]; then let "errors=errors+1"; echo -e "*** ERROR11\n\n"; fi
 ls -al isrp_samples.dat
 FILESIZE=$(stat -c%s "$FILENAME")
-if [[ $FILESIZE != $FILE_SIZE_512_FFT ]]; then let "file_size_errors=file_fize_error+1"; echo -e "\n***ERROR \n\n"; fi
+if [[ $FILESIZE != $FILE_SIZE_512_FFT ]]; then let "file_size_errors=file_size_error+1"; echo -e "\n***ERROR12 \n\n"; fi
 
 
 echo -e "\n***** psd fft_size=1024 channel = 1"
@@ -75,30 +76,30 @@ FILE_SIZE_1024_FFT=397312
 rm isrp_samples.dat
 ./rx_samples_to_file --dest_ip=10.75.42.248 --args=addr=10.75.42.202 --stream_type=psd --nsamps="$NUM_SAMPS" --fft_size=1024 --channel=1
 ret_code=$?
-if [ $ret_code != 0 ]; then let "errors=errors+1";  echo -e "*** ERROR\n\n"; fi
+if [ $ret_code != 0 ]; then let "errors=errors+1";  echo -e "*** ERROR13\n\n"; fi
 ls -al isrp_samples.dat
 FILESIZE=$(stat -c%s "$FILENAME")
-if [[ $FILESIZE != $FILE_SIZE_1024_FFT ]]; then let "file_size_errors=file_fize_error+1"; echo -e "\n***ERROR \n\n"; fi
+if [[ $FILESIZE != $FILE_SIZE_1024_FFT ]]; then let "file_size_errors=file_size_error+1"; echo -e "\n***ERROR14 \n\n"; fi
 
 echo -e "\n\n\n***** psd fft_size=1024 channel = 2"
 rm isrp_samples.dat
 ./rx_samples_to_file --dest_ip=10.75.42.248 --args=addr=10.75.42.202 --stream_type=psd --nsamps="$NUM_SAMPS" --fft_size=1024 --channel=2
 ret_code=$?
-if [ $ret_code != 0 ]; then let "errors=errors+1"; echo -e "*** ERROR \n\n"; fi
+if [ $ret_code != 0 ]; then let "errors=errors+1"; echo -e "*** ERROR15 \n\n"; fi
 ls -al isrp_samples.dat
 FILESIZE=$(stat -c%s "$FILENAME")
-if [[ $FILESIZE != $FILE_SIZE_1024_FFT ]]; then let "file_size_errors=file_fize_error+1"; echo -e "\n***ERROR \n\n"; fi
+if [[ $FILESIZE != $FILE_SIZE_1024_FFT ]]; then let "file_size_errors=file_size_error+1"; echo -e "\n***ERROR16 \n\n"; fi
 
 
 echo -e "\n\n\n***** psd fft_size=1024 channel = 3"
 rm isrp_samples.dat
 ./rx_samples_to_file --dest_ip=10.75.42.248 --args=addr=10.75.42.202 --stream_type=psd --nsamps="$NUM_SAMPS" --fft_size=1024 --channel=3
 ret_code=$?
-if [ $ret_code != 0 ]; then let "errors=errors+1"; echo -e "*** ERROR \n\n"; fi
+if [ $ret_code != 0 ]; then let "errors=errors+1"; echo -e "*** ERROR17 \n\n"; fi
 ls -al isrp_samples.dat
 rm isrp_samples.dat
 FILESIZE=$(stat -c%s "$FILENAME")
-if [[ $FILESIZE != $FILE_SIZE_1024_FFT ]]; then let "file_size_errors=file_fize_error+1"; echo -e "\n***ERROR \n\n"; fi
+if [[ $FILESIZE != $FILE_SIZE_1024_FFT ]]; then let "file_size_errors=file_size_error+1"; echo -e "\n***ERROR18 \n\n"; fi
 
 
 echo -e "\n\n\n***** psd fft_size=2048 channel = 1"
@@ -109,7 +110,7 @@ ret_code=$?
 if [ $ret_code != 0 ]; then let "errors=errors+1"; echo -e "**** ERROR \n\n"; fi
 ls -al isrp_samples.dat
 FILESIZE=$(stat -c%s "$FILENAME")
-if [[ $FILESIZE != $FILE_SIZE_2048_FFT ]]; then let "file_size_errors=file_fize_error+1"; echo -e "\n***ERROR \n\n"; fi
+if [[ $FILESIZE != $FILE_SIZE_2048_FFT ]]; then let "file_size_errors=file_size_error+1"; echo -e "\n***ERROR \n\n"; fi
 
 echo -e "\n\n\n***** psd fft_size=2048 channel = 2"
 rm isrp_samples.dat
@@ -118,7 +119,7 @@ ret_code=$?
 if [ $ret_code != 0 ]; then let "errors=errors+1"; echo -e "**** ERROR \n\n"; fi
 ls -al isrp_samples.dat
 FILESIZE=$(stat -c%s "$FILENAME")
-if [[ $FILESIZE != $FILE_SIZE_2048_FFT ]]; then let "file_size_errors=file_fize_error+1"; echo -e "\n***ERROR \n\n"; fi
+if [[ $FILESIZE != $FILE_SIZE_2048_FFT ]]; then let "file_size_errors=file_size_error+1"; echo -e "\n***ERROR \n\n"; fi
 
 
 echo -e "\n\n\n***** psd fft_size=2048 channel = 3"
@@ -128,7 +129,7 @@ ret_code=$?
 if [ $ret_code != 0 ]; then let "errors=errors+1"; echo -e "**** ERROR \n\n"; fi
 ls -al isrp_samples.dat
 FILESIZE=$(stat -c%s "$FILENAME")
-if [[ $FILESIZE != $FILE_SIZE_2048_FFT ]]; then let "file_size_errors=file_fize_error+1"; echo -e "\n***ERROR \n\n"; fi
+if [[ $FILESIZE != $FILE_SIZE_2048_FFT ]]; then let "file_size_errors=file_size_error+1"; echo -e "\n***ERROR \n\n"; fi
 
 
 echo -e "\n\n\n***** psd fft_size=4096 channel = 1"
@@ -139,7 +140,7 @@ ret_code=$?
 if [ $ret_code != 0 ]; then let "errors=errors+1"; echo -e "**** ERROR \n\n"; fi
 ls -al isrp_samples.dat
 FILESIZE=$(stat -c%s "$FILENAME")
-if [[ $FILESIZE != $FILE_SIZE_4096_FFT ]]; then let "file_size_errors=file_fize_error+1"; echo -e "\n***ERROR \n\n"; fi
+if [[ $FILESIZE != $FILE_SIZE_4096_FFT ]]; then let "file_size_errors=file_size_error+1"; echo -e "\n***ERROR \n\n"; fi
 
 echo -e "\n\n\n***** psd fft_size=4096 channel = 2"
 rm isrp_samples.dat
@@ -148,7 +149,7 @@ ret_code=$?
 if [ $ret_code != 0 ]; then let "errors=errors+1"; echo -e "**** ERROR \n\n"; fi
 ls -al isrp_samples.dat
 FILESIZE=$(stat -c%s "$FILENAME")
-if [[ $FILESIZE != $FILE_SIZE_4096_FFT ]]; then let "file_size_errors=file_fize_error+1"; echo -e "\n***ERROR \n\n"; fi
+if [[ $FILESIZE != $FILE_SIZE_4096_FFT ]]; then let "file_size_errors=file_size_error+1"; echo -e "\n***ERROR \n\n"; fi
 
 
 echo -e "\n\n\n***** psd fft_size=4096 channel = 3"
@@ -158,7 +159,7 @@ ret_code=$?
 if [ $ret_code != 0 ]; then let "errors=errors+1"; echo -e "**** ERROR \n\n"; fi
 ls -al isrp_samples.dat
 FILESIZE=$(stat -c%s "$FILENAME")
-if [[ $FILESIZE != $FILE_SIZE_4096_FFT ]]; then let "file_size_errors=file_fize_error+1"; echo -e "\n***ERROR \n\n"; fi
+if [[ $FILESIZE != $FILE_SIZE_4096_FFT ]]; then let "file_size_errors=file_size_error+1"; echo -e "\n***ERROR \n\n"; fi
 
 
 echo "ERRORS = ${errors}"
