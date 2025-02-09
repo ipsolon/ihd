8cdbd0b5f0a00e094a8c348a98cd5e6da70c85a2
jplaschke <john.plaschke@bluehalo.com>
2025-02-09 17:19:13 -0500

    Initial test_start_sop

--- test_start_stop.cpp
+++ test_start_stop.cpp
@@ -6,7 +6,6 @@
 
 #include <fcntl.h>
 #include <sys/types.h>
-#include <unistd.h>
 #include <sys/stat.h>
 
 #include <iostream>
@@ -30,6 +29,7 @@ public:
         stream_args.channels = channel_nums;
         rx_stream = isrp->get_rx_stream(stream_args);
     }
+    virtual ~RxStream() = default;
 
     // Start a threads to receive iq/psd data - one for each channel enabled in the chan_mask
     virtual void run_loop() = 0;
@@ -51,6 +51,25 @@ public:
         rx_stream->issue_stream_cmd(stream_cmd);
     }
 
+    void start_stream() {
+        std::cout << "Run stream for channel:" << channel << std::endl;
+
+        /************************************************************************
+         * Start the stream
+         ***********************************************************************/
+        uhd::stream_cmd_t stream_cmd(uhd::stream_cmd_t::STREAM_MODE_START_CONTINUOUS);
+        rx_stream->issue_stream_cmd(stream_cmd);
+    }
+
+    void stop_stream() {
+        /************************************************************************
+         * Receive Data
+         ***********************************************************************/
+        uhd::stream_cmd_t stream_cmd(uhd::stream_cmd_t::STREAM_MODE_STOP_CONTINUOUS);
+        rx_stream->issue_stream_cmd(stream_cmd);
+    }
+
+
 private:
     uhd::stream_args_t stream_args{};
 
@@ -66,7 +85,7 @@ public:
         : RxStream(chan, isrp, tt, stream_args) {
     }
 
-    virtual ~RxStreamPsd() = default;
+    ~RxStreamPsd() override = default;
 
 private:
     void run_loop() override {
@@ -114,7 +133,7 @@ public:
         : RxStream(chan, isrp, tt, stream_args) {
     }
 
-    virtual ~RxStreamIq() = default;
+    ~RxStreamIq() override = default;
 
 private:
     void run_loop() override {
@@ -161,8 +180,7 @@ private:
 int IHD_SAFE_MAIN(int argc, char *argv[]) {
     std::cout << "Revision: " << ihd::get_version_string() << std::endl;
     std::string args;
-    size_t chan_mask;
-    uint32_t total_time;
+    uint32_t total_time = 30;
 
     std::string dest_ip;
     uint16_t dest_port;
@@ -173,15 +191,15 @@ int IHD_SAFE_MAIN(int argc, char *argv[]) {
     po::options_description desc("Allowed options");
     desc.add_options()
             ("help", "help message")
-            ("duration", po::value<uint32_t>(&total_time)->default_value(30), "total number of seconds to receive")
-            ("chan_mask", po::value<size_t>(&chan_mask)->default_value(1),
-             "channel mask (chan 1 = 0x1, chan 2 = 0x2, chan 1 & 2 = 0x3)")
+            //("duration", po::value<uint32_t>(&total_time)->default_value(30), "total number of seconds to receive")
+            //("chan_mask", po::value<size_t>(&chan_mask)->default_value(1),
+            // "channel mask (chan 1 = 0x1, chan 2 = 0x2, chan 1 & 2 = 0x3)")
             ("dest_ip", po::value<std::string>(&dest_ip)->default_value("0.0.0.0"), "destination IP address")
             ("dest_port", po::value<uint16_t>(&dest_port)->default_value(9090), "destination port")
-            ("fft_size", po::value<uint32_t>(&fft_size)->default_value(256), "FFT size (256, 512, 1024, 2048 or 4096")
-            ("fft_avg", po::value<uint32_t>(&fft_avg)->default_value(105), "FFT averaging count")
-            ("args", po::value<std::string>(&args)->default_value(""), "ISRP device address args")
-            ("stream_type", po::value<std::string>(&stream_type)->default_value("psd"), "Stream type - (psd or iq)");
+            //("fft_size", po::value<uint32_t>(&fft_size)->default_value(256), "FFT size (256, 512, 1024, 2048 or 4096")
+            //("fft_avg", po::value<uint32_t>(&fft_avg)->default_value(105), "FFT averaging count")
+            ("args", po::value<std::string>(&args)->default_value(""), "ISRP device address args");
+    //("stream_type", po::value<std::string>(&stream_type)->default_value("psd"), "Stream type - (psd or iq)");
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, desc), vm);
     po::notify(vm);
@@ -193,7 +211,7 @@ int IHD_SAFE_MAIN(int argc, char *argv[]) {
                 << std::endl
                 << "This application streams data from a single channel of a Ipsolon device to a file.\n"
                 << std::endl;
-        return ~0;
+        return 0;
     }
     if (vm["args"].defaulted()) {
         std::cout
@@ -212,6 +230,67 @@ int IHD_SAFE_MAIN(int argc, char *argv[]) {
 
     ihd::ipsolon_isrp::sptr isrp = ihd::ipsolon_isrp::make(args);
 
+    std::vector<std::thread *> thread_vector;
+    std::vector<RxStream *> stream_vector;
+    // Create four streams - two iq and two psd
+
+    // First IQ stream
+    uhd::stream_args_t stream_args1("sc16", "sc16");
+    stream_args1.args[ihd::ipsolon_rx_stream::stream_type::STREAM_FORMAT_KEY] =
+               ihd::ipsolon_rx_stream::stream_type::IQ_STREAM;
+    stream_args1.args[ihd::ipsolon_rx_stream::stream_type::STREAM_DEST_IP_KEY] =
+            dest_ip;
+    stream_args1.args[ihd::ipsolon_rx_stream::stream_type::STREAM_DEST_PORT_KEY] =
+            std::to_string(dest_port);
+
+    stream_args1.args[ihd::ipsolon_rx_stream::stream_type::STREAM_DEST_PORT_KEY] = std::to_string(dest_port);
+    std::thread *thread_obj1;
+    RxStream *rxStream1;
+    rxStream1 = new RxStreamIq(1, isrp, total_time, stream_args1);
+    //thread_obj1 = new std::thread(&RxStream::start_stream, rxStream1);  // NOLINT
+    //thread_vector.push_back(thread_obj1);
+    //stream_vector.push_back(rxStream1);
+
+    // Second IQ stream
+    uhd::stream_args_t stream_args2("sc16", "sc16");
+    stream_args2.args[ihd::ipsolon_rx_stream::stream_type::STREAM_FORMAT_KEY] =
+               ihd::ipsolon_rx_stream::stream_type::IQ_STREAM;
+    stream_args2.args[ihd::ipsolon_rx_stream::stream_type::STREAM_DEST_IP_KEY] =
+            dest_ip;
+    stream_args2.args[ihd::ipsolon_rx_stream::stream_type::STREAM_DEST_PORT_KEY] =
+            std::to_string(dest_port+1);
+
+    stream_args2.args[ihd::ipsolon_rx_stream::stream_type::STREAM_DEST_PORT_KEY] = std::to_string(dest_port);
+    std::thread *thread_obj2;
+    RxStream *rxStream2;
+    rxStream2 = new RxStreamIq(2, isrp, total_time, stream_args2);
+    //thread_obj1 = new std::thread(&RxStream::start_stream, rxStream2);  // NOLINT
+    //thread_vector.push_back(thread_obj2);
+    //stream_vector.push_back(rxStream2);
+    for (int i=0; i<5; i++) {
+        rxStream1->start_stream();
+        std::cout << "AFTER start_stream1 loop " << i << std::endl;
+        rxStream2->start_stream();
+        std::cout << "AFTER start_stream2" << std::endl;
+        usleep(1000000);
+        rxStream1->stop_stream();
+        std::cout << "AFTER stop_stream1" << std::endl;
+        rxStream2->stop_stream();
+        std::cout << "AFTER stop_stream2" << std::endl;
+      }
+    free(rxStream1);
+    free(rxStream2);
+#if 0
+        for (auto t: thread_vector) {
+            t->join();
+            free(t);
+        }
+        for (auto s: stream_vector) {
+            free(s);
+        }
+#endif
+
+#if 0
     /************************************************************************
      * Get Rx Stream
      ***********************************************************************/
@@ -274,5 +353,7 @@ int IHD_SAFE_MAIN(int argc, char *argv[]) {
     for (auto s: stream_vector) {
         free(s);
     }
+#endif
+
     return (0);
 }
