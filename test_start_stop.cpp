f7dffa3c7b835afc9dc4b629b7250739c4157069
jplaschke <john.plaschke@bluehalo.com>
2025-02-10 17:01:37 -0500

    cleanup test_start_stop.cpp

--- test_start_stop.cpp
+++ test_start_stop.cpp
@@ -16,13 +16,15 @@
 #include "safe_main.hpp"
 #include "ihd.h"
 
+#include "debug.hpp"
+
 namespace po = boost::program_options;
 
 #define NUMBER_OF_CHANNELS          1 /* We are limited to a single channel per stream right now */
 
 class RxStream {
 public:
-    RxStream(size_t chan, ihd::ipsolon_isrp::sptr &isrp, uint32_t tt,
+    RxStream(size_t chan, const ihd::ipsolon_isrp::sptr &isrp, uint32_t tt,
              uhd::stream_args_t &stream_args) : channel(chan), total_time(tt) {
         std::vector<size_t> channel_nums;
         channel_nums.push_back(chan);
@@ -34,38 +36,23 @@ public:
     // Start a threads to receive iq/psd data - one for each channel enabled in the chan_mask
     virtual void run_loop() = 0;
 
-    void stream_run() {
-        std::cout << "Run stream for channel:" << channel << std::endl;
+    static constexpr uint64_t BYTES_PER_CHDR = 16;
+
+    void start_stream() const {
+        std::cout << "start_stream stream for channel:" << channel << std::endl;
 
         /************************************************************************
          * Start the stream
          ***********************************************************************/
-        uhd::stream_cmd_t stream_cmd(uhd::stream_cmd_t::STREAM_MODE_START_CONTINUOUS);
-        rx_stream->issue_stream_cmd(stream_cmd);
-
-        /************************************************************************
-         * Receive Data
-         ***********************************************************************/
-        run_loop();
-        stream_cmd.stream_mode = uhd::stream_cmd_t::STREAM_MODE_STOP_CONTINUOUS;
+        uhd::stream_cmd_t const stream_cmd(uhd::stream_cmd_t::STREAM_MODE_START_CONTINUOUS);
         rx_stream->issue_stream_cmd(stream_cmd);
     }
 
-    void start_stream() {
-        std::cout << "Run stream for channel:" << channel << std::endl;
-
+    void stop_stream() const {
         /************************************************************************
-         * Start the stream
+         * Stop the stream
          ***********************************************************************/
-        uhd::stream_cmd_t stream_cmd(uhd::stream_cmd_t::STREAM_MODE_START_CONTINUOUS);
-        rx_stream->issue_stream_cmd(stream_cmd);
-    }
-
-    void stop_stream() {
-        /************************************************************************
-         * Receive Data
-         ***********************************************************************/
-        uhd::stream_cmd_t stream_cmd(uhd::stream_cmd_t::STREAM_MODE_STOP_CONTINUOUS);
+        uhd::stream_cmd_t const stream_cmd(uhd::stream_cmd_t::STREAM_MODE_STOP_CONTINUOUS);
         rx_stream->issue_stream_cmd(stream_cmd);
     }
 
@@ -81,7 +68,7 @@ protected:
 
 class RxStreamPsd : public RxStream {
 public:
-    RxStreamPsd(size_t chan, ihd::ipsolon_isrp::sptr &isrp, uint32_t tt, uhd::stream_args_t &stream_args)
+    RxStreamPsd(size_t chan, const ihd::ipsolon_isrp::sptr &isrp, uint32_t tt, uhd::stream_args_t &stream_args)
         : RxStream(chan, isrp, tt, stream_args) {
     }
 
@@ -94,7 +81,7 @@ private:
          * Allocate buffers
          ***********************************************************************/
         std::vector<std::complex<int16_t> *> buffs(NUMBER_OF_CHANNELS);
-        size_t spb = rx_stream->get_max_num_samps();
+        size_t const spb = rx_stream->get_max_num_samps();
         std::complex<int16_t> p[spb];
         buffs[0] = p;
         auto startTime = std::chrono::high_resolution_clock::now();
@@ -104,7 +91,7 @@ private:
         uint64_t packets = 0;
         uint64_t bytes = 0;
         while (std::chrono::high_resolution_clock::now() - startTime < duration) {
-            size_t n = rx_stream->recv(buffs, spb, md, 5);
+            size_t const n = rx_stream->recv(buffs, spb, md, 5);
             if (md.out_of_sequence) {
                 fprintf(stderr, "*** OUT OF SEQUENCE PACKET:\n%s\n***\n", md.to_pp_string(false).c_str());
                 errors++;
@@ -120,8 +107,8 @@ private:
         auto finishTime = std::chrono::duration_cast<std::chrono::milliseconds>(
             std::chrono::high_resolution_clock::now() - startTime);
         bytes *= 4; /* bytes = samples * 4 */
-        bytes += 16 * packets; /* Account for CHDR */
-        double megabits_per_second = (((double) bytes / total_time) / (1024 * 1024)) * 8;
+        bytes += BYTES_PER_CHDR * packets; /* Account for CHDR */
+        double const megabits_per_second = ((static_cast<double>(bytes) / total_time) / (1024 * 1024)) * 8;
         printf("RESULT chan:%zu duration ms:%ld packets:%lu bytes:%lu Mb/s:%f errors:%lu\n",
                channel, finishTime.count(), packets, bytes, megabits_per_second, errors);
     }
@@ -129,7 +116,7 @@ private:
 
 class RxStreamIq : public RxStream {
 public:
-    RxStreamIq(size_t chan, ihd::ipsolon_isrp::sptr &isrp, uint32_t tt, uhd::stream_args_t &stream_args)
+    RxStreamIq(size_t chan, const ihd::ipsolon_isrp::sptr &isrp, uint32_t tt, uhd::stream_args_t &stream_args)
         : RxStream(chan, isrp, tt, stream_args) {
     }
 
@@ -142,7 +129,7 @@ private:
          * Allocate buffers
          ***********************************************************************/
         std::vector<std::complex<int16_t> *> buffs(NUMBER_OF_CHANNELS);
-        size_t spb = rx_stream->get_max_num_samps();
+        size_t const spb = rx_stream->get_max_num_samps();
         std::complex<int16_t> p[spb];
         buffs[0] = p;
         auto startTime = std::chrono::high_resolution_clock::now();
@@ -153,7 +140,7 @@ private:
         uint64_t bytes = 0;
         // FIXME - TEMP_PACKET_LIMIT is temporary until udp keeps up with dma
         while ((std::chrono::high_resolution_clock::now() - startTime < duration) && (packets < TEMP_PACKET_LIMIT)) {
-            size_t n = rx_stream->recv(buffs, spb, md, 5);
+            size_t const n = rx_stream->recv(buffs, spb, md, 5);
             if (md.out_of_sequence) {
                 fprintf(stderr, "*** OUT OF SEQUENCE PACKET:\n%s\n***\n", md.to_pp_string(false).c_str());
                 errors++;
@@ -169,8 +156,8 @@ private:
         auto finishTime = std::chrono::duration_cast<std::chrono::milliseconds>(
             std::chrono::high_resolution_clock::now() - startTime);
         bytes *= 4; /* bytes = samples * 4 */
-        bytes += 16 * packets; /* Account for CHDR */
-        double megabits_per_second = (((double) bytes / total_time) / (1024 * 1024)) * 8;
+        bytes += BYTES_PER_CHDR * packets; /* Account for CHDR */
+        double const megabits_per_second = ((static_cast<double>(bytes) / total_time) / (1024 * 1024)) * 8;
         printf("RESULT chan:%zu duration ms:%ld packets:%lu bytes:%lu Mb/s:%f errors:%lu\n",
                channel, finishTime.count(), packets, bytes, megabits_per_second, errors);
     }
@@ -179,27 +166,26 @@ private:
 
 int IHD_SAFE_MAIN(int argc, char *argv[]) {
     std::cout << "Revision: " << ihd::get_version_string() << std::endl;
+    constexpr int DEFAULT_NUM_LOOPS = 5;
+    constexpr uint16_t DEFAULT_PORT = 9090;
+
     std::string args;
-    uint32_t total_time = 30;
+    constexpr uint32_t total_time = 2;
+    uint32_t num_loops = 2;
 
     std::string dest_ip;
-    uint16_t dest_port;
-    uint32_t fft_size;
-    uint32_t fft_avg;
-    std::string stream_type;
+    uint16_t dest_port = 0;
+    constexpr uint32_t fft_size = 256;
+    constexpr uint32_t fft_avg = 105;
+    std::string const stream_type;
 
     po::options_description desc("Allowed options");
     desc.add_options()
             ("help", "help message")
-            //("duration", po::value<uint32_t>(&total_time)->default_value(30), "total number of seconds to receive")
-            //("chan_mask", po::value<size_t>(&chan_mask)->default_value(1),
-            // "channel mask (chan 1 = 0x1, chan 2 = 0x2, chan 1 & 2 = 0x3)")
+            ("num_loops", po::value<uint32_t>(&num_loops)->default_value(DEFAULT_NUM_LOOPS), "Number of start stop loops")
             ("dest_ip", po::value<std::string>(&dest_ip)->default_value("0.0.0.0"), "destination IP address")
-            ("dest_port", po::value<uint16_t>(&dest_port)->default_value(9090), "destination port")
-            //("fft_size", po::value<uint32_t>(&fft_size)->default_value(256), "FFT size (256, 512, 1024, 2048 or 4096")
-            //("fft_avg", po::value<uint32_t>(&fft_avg)->default_value(105), "FFT averaging count")
-            ("args", po::value<std::string>(&args)->default_value(""), "ISRP device address args");
-    //("stream_type", po::value<std::string>(&stream_type)->default_value("psd"), "Stream type - (psd or iq)");
+            ("dest_port", po::value<uint16_t>(&dest_port)->default_value(DEFAULT_PORT), "destination port")
+            ("args", po::value<std::string>(&args)->default_value(""), "USRP device address args");
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, desc), vm);
     po::notify(vm);
@@ -228,10 +214,10 @@ int IHD_SAFE_MAIN(int argc, char *argv[]) {
     std::cout << std::endl;
     std::cout << boost::format("Creating the ISRP device with: %s...") % args << std::endl;
 
-    ihd::ipsolon_isrp::sptr isrp = ihd::ipsolon_isrp::make(args);
+    ihd::ipsolon_isrp::sptr const isrp = ihd::ipsolon_isrp::make(args);
 
-    std::vector<std::thread *> thread_vector;
-    std::vector<RxStream *> stream_vector;
+    std::vector<std::thread *> const thread_vector;
+    std::vector<RxStream *> const stream_vector;
     // Create four streams - two iq and two psd
 
     // First IQ stream
@@ -244,12 +230,8 @@ int IHD_SAFE_MAIN(int argc, char *argv[]) {
             std::to_string(dest_port);
 
     stream_args1.args[ihd::ipsolon_rx_stream::stream_type::STREAM_DEST_PORT_KEY] = std::to_string(dest_port);
-    std::thread *thread_obj1;
-    RxStream *rxStream1;
+    RxStream *rxStream1 = nullptr;
     rxStream1 = new RxStreamIq(1, isrp, total_time, stream_args1);
-    //thread_obj1 = new std::thread(&RxStream::start_stream, rxStream1);  // NOLINT
-    //thread_vector.push_back(thread_obj1);
-    //stream_vector.push_back(rxStream1);
 
     // Second IQ stream
     uhd::stream_args_t stream_args2("sc16", "sc16");
@@ -258,102 +240,63 @@ int IHD_SAFE_MAIN(int argc, char *argv[]) {
     stream_args2.args[ihd::ipsolon_rx_stream::stream_type::STREAM_DEST_IP_KEY] =
             dest_ip;
     stream_args2.args[ihd::ipsolon_rx_stream::stream_type::STREAM_DEST_PORT_KEY] =
-            std::to_string(dest_port+1);
+           std::to_string(dest_port+1);
 
     stream_args2.args[ihd::ipsolon_rx_stream::stream_type::STREAM_DEST_PORT_KEY] = std::to_string(dest_port);
-    std::thread *thread_obj2;
-    RxStream *rxStream2;
+    RxStream *rxStream2 = nullptr;
     rxStream2 = new RxStreamIq(2, isrp, total_time, stream_args2);
-    //thread_obj1 = new std::thread(&RxStream::start_stream, rxStream2);  // NOLINT
-    //thread_vector.push_back(thread_obj2);
-    //stream_vector.push_back(rxStream2);
-    for (int i=0; i<5; i++) {
+
+    // Third stream is PSD
+    uhd::stream_args_t stream_args3("sc16", "sc16");
+    stream_args3.args[ihd::ipsolon_rx_stream::stream_type::STREAM_FORMAT_KEY] =
+                   ihd::ipsolon_rx_stream::stream_type::PSD_STREAM;
+    stream_args3.args[ihd::ipsolon_rx_stream::stream_type::STREAM_DEST_IP_KEY] =
+           dest_ip;
+    stream_args3.args[ihd::ipsolon_rx_stream::stream_type::STREAM_DEST_PORT_KEY] =
+            std::to_string(dest_port+2);
+    stream_args3.args[ihd::ipsolon_rx_stream::stream_type::FFT_SIZE_KEY] =
+            std::to_string(fft_size);
+    stream_args3.args[ihd::ipsolon_rx_stream::stream_type::FFT_AVG_COUNT_KEY] =
+            std::to_string(fft_avg);
+    stream_args3.args[ihd::ipsolon_rx_stream::stream_type::STREAM_DEST_PORT_KEY] = std::to_string(dest_port);
+    RxStream *rxStream3 = nullptr;
+    rxStream3 = new RxStreamPsd(3, isrp, total_time, stream_args3);
+
+    // Fourth stream is PSD
+    uhd::stream_args_t stream_args4("sc16", "sc16");
+    stream_args4.args[ihd::ipsolon_rx_stream::stream_type::STREAM_FORMAT_KEY] =
+                   ihd::ipsolon_rx_stream::stream_type::PSD_STREAM;
+    stream_args4.args[ihd::ipsolon_rx_stream::stream_type::STREAM_DEST_IP_KEY] =
+            dest_ip;
+    stream_args4.args[ihd::ipsolon_rx_stream::stream_type::STREAM_DEST_PORT_KEY] =
+            std::to_string(dest_port+3);
+    stream_args4.args[ihd::ipsolon_rx_stream::stream_type::FFT_SIZE_KEY] =
+            std::to_string(fft_size);
+    stream_args4.args[ihd::ipsolon_rx_stream::stream_type::FFT_AVG_COUNT_KEY] =
+            std::to_string(fft_avg);
+
+    stream_args4.args[ihd::ipsolon_rx_stream::stream_type::STREAM_DEST_PORT_KEY] = std::to_string(dest_port);
+    RxStream *rxStream4 = nullptr;
+    rxStream4 = new RxStreamPsd(4, isrp, total_time, stream_args4);
+
+    // Fourth stream is PSD
+    for (int i=0; i<num_loops; i++) {
+        constexpr int ONE_SECOND = 1000000;
         rxStream1->start_stream();
-        std::cout << "AFTER start_stream1 loop " << i << std::endl;
         rxStream2->start_stream();
-        std::cout << "AFTER start_stream2" << std::endl;
-        usleep(1000000);
+        rxStream3->start_stream();
+        rxStream4->start_stream();
+        usleep(ONE_SECOND);
         rxStream1->stop_stream();
-        std::cout << "AFTER stop_stream1" << std::endl;
         rxStream2->stop_stream();
-        std::cout << "AFTER stop_stream2" << std::endl;
+        rxStream3->stop_stream();
+        rxStream4->stop_stream();
       }
-    free(rxStream1);
-    free(rxStream2);
-#if 0
-        for (auto t: thread_vector) {
-            t->join();
-            free(t);
-        }
-        for (auto s: stream_vector) {
-            free(s);
-        }
-#endif
-
-#if 0
-    /************************************************************************
-     * Get Rx Stream
-     ***********************************************************************/
-    uhd::stream_args_t stream_args("sc16", "sc16");
-    if (stream_type == ihd::ipsolon_rx_stream::stream_type::PSD_STREAM) {
-        stream_args.args[ihd::ipsolon_rx_stream::stream_type::STREAM_FORMAT_KEY] =
-                ihd::ipsolon_rx_stream::stream_type::PSD_STREAM;
-        stream_args.args[ihd::ipsolon_rx_stream::stream_type::STREAM_DEST_IP_KEY] =
-                dest_ip;
-        stream_args.args[ihd::ipsolon_rx_stream::stream_type::STREAM_DEST_PORT_KEY] =
-                std::to_string(dest_port);
-        stream_args.args[ihd::ipsolon_rx_stream::stream_type::FFT_SIZE_KEY] =
-                std::to_string(fft_size);
-        stream_args.args[ihd::ipsolon_rx_stream::stream_type::FFT_AVG_COUNT_KEY] =
-                std::to_string(fft_avg);
-    } else if (stream_type == ihd::ipsolon_rx_stream::stream_type::IQ_STREAM) {
-        stream_args.args[ihd::ipsolon_rx_stream::stream_type::STREAM_FORMAT_KEY] =
-                ihd::ipsolon_rx_stream::stream_type::IQ_STREAM;
-        stream_args.args[ihd::ipsolon_rx_stream::stream_type::STREAM_DEST_IP_KEY] =
-                dest_ip;
-        stream_args.args[ihd::ipsolon_rx_stream::stream_type::STREAM_DEST_PORT_KEY] =
-                std::to_string(dest_port);
-    } else {
-        fprintf(stderr, "Error: Invalid stream_type\n");
-        exit(1);
-    }
-
-    std::vector<std::thread *> thread_vector;
-    std::vector<RxStream *> stream_vector;
-    int chan = 1;
-
-    while (chan_mask != 0) {
-        size_t current_mask = 1 << (chan - 1);
-        if (current_mask & chan_mask) {
-            stream_args.args[ihd::ipsolon_rx_stream::stream_type::STREAM_DEST_PORT_KEY] = std::to_string(dest_port);
-            std::thread *thread_obj;
-            RxStream *rxStream;
-            if (stream_type == ihd::ipsolon_rx_stream::stream_type::PSD_STREAM) {
-                rxStream = new RxStreamPsd(chan, isrp, total_time, stream_args);
-                thread_obj = new std::thread(&RxStream::stream_run, rxStream);
-            } else {
-                rxStream = new RxStreamIq(chan, isrp, total_time, stream_args);
-                thread_obj = new std::thread(&RxStream::stream_run, rxStream);
-            }
-            thread_vector.push_back(thread_obj);
-            stream_vector.push_back(rxStream);
-            chan_mask &= ~(current_mask);
-            dest_port++;
-        }
-        chan++;
-        if (chan > ihd::ipsolon_rx_stream::MAX_RX_CHANNELS) {
-            chan_mask = 0;
-        }
-    }
-
-    for (auto t: thread_vector) {
-        t->join();
-        free(t);
-    }
-    for (auto s: stream_vector) {
-        free(s);
-    }
-#endif
+    dbprintf("BEFORE delete stream\n");
+    delete rxStream1;
+    delete rxStream2;
+    delete rxStream3;
+    delete rxStream4;
 
     return (0);
 }
